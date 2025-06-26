78d675b8169d5b4f8ea357f2a51fc026a87a1403
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2025-06-26 11:24:56 +0200

    add entry points for parsers

--- {{cookiecutter.plugin_name}}/py_sources/src/parsers/__init__.py
+++ {{cookiecutter.plugin_name}}/py_sources/src/parsers/__init__.py
@@ -16,3 +16,35 @@ parser_entry_point = NewParserEntryPoint(
     description='New parser entry point configuration.',
     mainfile_name_re=r'.*\.newmainfilename',
 )
+
+
+class {{cookiecutter.lab_name}}ExperimentParserEntryPoint(ParserEntryPoint):
+    # parameter: int = Field(0, description='Custom configuration parameter')
+
+    def load(self):
+        from {{cookiecutter.module_name}}.parsers.{{cookiecutter.lab_name}}_batch_parser import {{cookiecutter.lab_name}}ExperimentParser
+
+        return {{cookiecutter.lab_name}}ExperimentParser(**self.model_dump())
+
+
+parser_entry_point = {{cookiecutter.lab_name}}ExperimentParserEntryPoint(
+    name='{{cookiecutter.lab_name}}ExperimentParserEntryPoint',
+    description='{{cookiecutter.lab_name}} experiment parser entry point configuration.',
+    mainfile_name_re=r'.*\.newmainfilename',
+)
+
+
+class {{cookiecutter.lab_name}}ParserEntryPoint(ParserEntryPoint):
+    parameter: int = Field(0, description='Custom configuration parameter')
+
+    def load(self):
+        from {{cookiecutter.module_name}}.parsers.{{cookiecutter.lab_name}}_measurement_parser import {{cookiecutter.lab_name}}Parser
+
+        return {{cookiecutter.lab_name}}Parser(**self.model_dump())
+
+
+parser_entry_point = {{cookiecutter.lab_name}}ParserEntryPoint(
+    name='{{cookiecutter.lab_name}}ParserEntryPoint',
+    description='{{cookiecutter.lab_name}} parser entry point configuration.',
+    mainfile_name_re=r'.*\.newmainfilename',
+)
