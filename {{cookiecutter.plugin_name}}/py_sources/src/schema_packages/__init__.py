f0ed165ca1d032bf20ce23b5e1e0c26eb2edab19
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2025-06-26 13:38:25 +0200

    reorganize entry points registration

--- {{cookiecutter.plugin_name}}/py_sources/src/schema_packages/__init__.py
+++ {{cookiecutter.plugin_name}}/py_sources/src/schema_packages/__init__.py
@@ -11,12 +11,6 @@ class NewSchemaPackageEntryPoint(SchemaPackageEntryPoint):
         return m_package
 
 
-schema_package_entry_point = NewSchemaPackageEntryPoint(
-    name='NewSchemaPackage',
-    description='New schema package entry point configuration.',
-)
-
-
 class {{cookiecutter.lab_name}}PackageEntryPoint(SchemaPackageEntryPoint):
     parameter: int = Field(0, description='Custom configuration parameter')
 
@@ -26,7 +20,13 @@ class {{cookiecutter.lab_name}}PackageEntryPoint(SchemaPackageEntryPoint):
         return m_package
 
 
-schema_package_entry_point = {{cookiecutter.lab_name}}PackageEntryPoint(
+schema_package_entry_point = NewSchemaPackageEntryPoint(
+    name='NewSchemaPackage',
+    description='New schema package entry point configuration.',
+)
+
+
+{{cookiecutter.lab_name}}_schema_package_entry_point = {{cookiecutter.lab_name}}PackageEntryPoint(
     name='{{cookiecutter.lab_name}}Package',
     description='{{cookiecutter.lab_name}} package entry point configuration.',
 )
