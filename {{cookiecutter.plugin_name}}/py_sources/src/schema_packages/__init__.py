d79e0686b555219fee031c557fd7f3f857007b48
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2025-06-26 11:31:25 +0200

    add entry point for schema package

--- {{cookiecutter.plugin_name}}/py_sources/src/schema_packages/__init__.py
+++ {{cookiecutter.plugin_name}}/py_sources/src/schema_packages/__init__.py
@@ -15,3 +15,18 @@ schema_package_entry_point = NewSchemaPackageEntryPoint(
     name='NewSchemaPackage',
     description='New schema package entry point configuration.',
 )
+
+
+class {{cookiecutter.lab_name}}PackageEntryPoint(SchemaPackageEntryPoint):
+    parameter: int = Field(0, description='Custom configuration parameter')
+
+    def load(self):
+        from {{cookiecutter.module_name}}.schema_packages.{{cookiecutter.lab_name}}_package import m_package
+
+        return m_package
+
+
+schema_package_entry_point = {{cookiecutter.lab_name}}PackageEntryPoint(
+    name='{{cookiecutter.lab_name}}Package',
+    description='{{cookiecutter.lab_name}} package entry point configuration.',
+)
